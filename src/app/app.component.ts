5a3cd522bc57a1151683201b8c4d3f54f7eec06b
Aitor Calderon <aitorclzamakola@gmail.com>
2023-12-24 14:17:54 +0100

    feat(app): Create vertical scroll navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {RouterOutlet} from '@angular/router';
 import {HeaderComponent} from "./header/header.component";
+import {HomeComponent} from "./home/home.component";
+import {AboutComponent} from "./about/about.component";
+import {WorkComponent} from "./work/work.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeaderComponent],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, HomeComponent, AboutComponent, WorkComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
