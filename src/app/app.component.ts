c84ed4b207fd30ac5d1a3a0df9e0d9044dec7eaa
Aitor Calderon <aitorclzamakola@gmail.com>
2023-12-31 12:40:44 +0100

    feat(footer): Create footer component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,11 +7,12 @@ import {AboutComponent} from "./about/about.component";
 import {WorkComponent} from "./work/work.component";
 import {ScrollService} from "./shared/services/scroll.service";
 import {ContactComponent} from "./contact/contact.component";
+import {FooterComponent} from "./footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeaderComponent, HomeComponent, AboutComponent, WorkComponent, ContactComponent],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, HomeComponent, AboutComponent, WorkComponent, ContactComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
