1e88e11619ef0388370a865216f0da6342565053
Aitor Calderon <aitorclzamakola@gmail.com>
2024-01-01 16:48:24 +0100

    feat(app): Refactor code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,4 @@
-import {AfterViewInit, Component, ElementRef, OnDestroy, ViewChild} from '@angular/core';
-import {CommonModule} from '@angular/common';
-import {RouterOutlet} from '@angular/router';
+import {AfterViewInit, Component, ElementRef, OnDestroy, QueryList, ViewChildren} from '@angular/core';
 import {HeaderComponent} from "./header/header.component";
 import {HomeComponent} from "./home/home.component";
 import {AboutComponent} from "./about/about.component";
@@ -8,49 +6,50 @@ import {WorkComponent} from "./work/work.component";
 import {ScrollService} from "./shared/services/scroll.service";
 import {ContactComponent} from "./contact/contact.component";
 import {FooterComponent} from "./footer/footer.component";
-import {Subscription} from "rxjs";
+import {fromEvent, Subject, takeUntil} from "rxjs";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeaderComponent, HomeComponent, AboutComponent, WorkComponent, ContactComponent, FooterComponent],
+  imports: [HeaderComponent, HomeComponent, AboutComponent, WorkComponent, ContactComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements AfterViewInit, OnDestroy {
 
-  private subscription: Subscription;
-
   title = 'Portfolio';
 
-  @ViewChild('scrollContainer') scrollContainer: ElementRef;
+  @ViewChildren('scrollContainer, home, about, work, contact, footer') sections: QueryList<ElementRef>;
 
-  @ViewChild('home') home: ElementRef;
-  @ViewChild('about') about: ElementRef;
-  @ViewChild('work') work: ElementRef;
-  @ViewChild('contact') contact: ElementRef;
-  @ViewChild('footer') footer: ElementRef;
+  private destroy$ = new Subject<void>();
 
   constructor(private scrollService: ScrollService) {
   }
 
   ngAfterViewInit() {
-    const containerElement = this.scrollContainer.nativeElement;
-
-    containerElement.addEventListener('scroll', () => {
-      this.scrollService.notifyScroll(containerElement.scrollTop);
-    });
-
-    this.subscription = this.scrollService.section$.subscribe(sectionId => {
-      this.scrollToElement(this[sectionId]);
-    });
-  }
-
-  ngOnDestroy() {
-    this.subscription.unsubscribe();
+    this.setupScrollListener();
+    this.subscribeToScrollService();
   }
 
   scrollToElement(element: ElementRef) {
     element.nativeElement.scrollIntoView({behavior: "smooth"});
+  }
+
+  ngOnDestroy() {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+
+  private setupScrollListener() {
+    const scrollContainerElement = this.sections.toArray()[0].nativeElement;
+    fromEvent(scrollContainerElement, 'scroll')
+      .pipe(takeUntil(this.destroy$))
+      .subscribe(() => this.scrollService.notifyScroll(scrollContainerElement.scrollTop));
+  }
+
+  private subscribeToScrollService() {
+    this.scrollService.section$
+      .pipe(takeUntil(this.destroy$))
+      .subscribe(sectionId => this.scrollToElement(this.sections.find(section => section.nativeElement.id === sectionId)));
   }
 }
