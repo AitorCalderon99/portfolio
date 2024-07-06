8e1393d2c8036ca653cb992286d166b988d8c82e
Aitor Calderon <aitorclzamakola@gmail.com>
2024-07-06 16:39:14 +0200

    feat(home): Home text animation start

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,13 +1,29 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {Component, OnInit} from '@angular/core';
+import {CommonModule, NgOptimizedImage} from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, NgOptimizedImage],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
+
+  texts = ['Aitor Calderon', 'Frontend Developer', 'Web Developer'];
+  mainText = this.texts[0];
+  counter = 0;
+  animateClass = 'animate__lightSpeedInRight';
+
+  ngOnInit() {
+    setInterval(() => {
+      this.animateClass = 'animate__lightSpeedOutRight';
+      setTimeout(() => {
+        this.counter = (this.counter + 1) % this.texts.length;
+        this.mainText = this.texts[this.counter];
+        this.animateClass = 'animate__lightSpeedInRight';
+      }, 1000);
+    }, 4500);
+  }
 
 }
